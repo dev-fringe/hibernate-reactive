26fa9b44a85ff4874a0ab0971fe5395fef28d901
hdlee <dev.fringe@gmail.com>
2022-09-19 22:14:08 +0900

    init

--- src/main/java/dev/fringe/hibenate/reactive/HibernateConfig.java
+++ src/main/java/dev/fringe/hibenate/reactive/HibernateConfig.java
@@ -7,6 +7,7 @@ import org.hibernate.reactive.mutiny.Mutiny;
 import org.hibernate.reactive.stage.Stage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 //@EnableTransactionManagement
@@ -28,10 +29,11 @@ public class HibernateConfig {
 //		return t;
 //	}
 
-//	@Bean
-//	public Mutiny.SessionFactory sessionFactory() {
-//		return Persistence.createEntityManagerFactory("ReactivePU").unwrap(Mutiny.SessionFactory.class);
-//	}
+	@Bean
+	@Primary
+	public Mutiny.SessionFactory sessionFactory() {
+		return Persistence.createEntityManagerFactory("ReactivePU").unwrap(Mutiny.SessionFactory.class);
+	}
 //	
     @Bean
     public EntityManagerFactory entityManagerFactory() {
